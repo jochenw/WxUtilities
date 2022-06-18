bca20937eb9053cb7401162530b2f1a14c38bff3
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2022-06-18 23:15:58 +0200

    Initial work on the WxLog services.

---
+++ src/main/IS/packages/WxLog2/code/source/wx/log2/pub/plain.java
@@ -0,0 +1,38 @@
+package wx.log2.pub;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class plain
+
+{
+	// ---( internal utility methods )---
+
+	final static plain _instance = new plain();
+
+	static plain _newInstance() { return new plain(); }
+
+	static plain _cast(Object o) { return (plain)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void logMessage (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(logMessage)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
